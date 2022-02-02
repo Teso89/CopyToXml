d1a8bd69e199a97632fd1ade7314771765360043
Daniel Alejandro Tesolin <daniel_at89@hotmail.com>
2022-02-02 17:24:26 -0300

    Cambios en la ayuda.

--- src/com/tesolin/copyToXml/main/Main.java
+++ src/com/tesolin/copyToXml/main/Main.java
@@ -34,22 +34,6 @@ public class Main {
     // Método para mostrar la explicación del programa.
     public static void muestraAyuda() {
 
-        /*
-        String chamullo = "\n" +
-                        "Instrucciones." + "\n" +
-                        "--------------" + "\n" +
-                        "\n" +
-                        "1) Poner uno o mas copys Cobol junto a este .JAR." + "\n" +
-                        "2) Abrir el copy y poner las marcas {{ENTRADA}} y {{SALIDA}} al principio de las variable de grupo." + "\n" +
-                        "   Para excluir una linea del procesamiento solo se debe agregar un * (asterisco) al principio." + "\n" +
-                        "Por ejemplo:" + "\n" +
-                        "            {{ENTRADA}}  05 HS27-DATOS-ENTRADA." + "\n" +
-                        "            {{SALIDA}}  05 HS27-RESPUESTA." + "\n" +
-                        "            * 15 HS27-TAR-CLA              PIC  X(19).  " + "\n" +
-                        "3) Correr el .JAR y esperar a obtener los .XML" + "\n"
-                ;
-        */
-
         String chamullo =
                 "Intrucciones:\n" +
                 "\n" +
